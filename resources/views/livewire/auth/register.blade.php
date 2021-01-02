a4c533137a08c3b453c6a58c588dbab80c337e63
abela <abelardhana96@gmail.com>
2021-01-02 11:34:50 +0800

    lesson 9 completed

--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -1,3 +1,49 @@
-<div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
-</div>
+<form wire:submit.prevent="register">
+    <div>
+        <label for="name">
+            Name
+        </label>
+        <input
+          wire:model="name"
+          type="text"
+          id="name"
+          name="name"
+        >
+    </div>
+    <div>
+        <label for="email">
+            Email
+        </label>
+        <input
+          wire:model="email"
+          type="email"
+          id="email"
+          name="email"
+        >
+    </div>
+    <div>
+        <label for="password">
+            Password
+        </label>
+        <input
+          wire:model="password"
+          type="password"
+          id="password"
+          name="password"
+        >
+    </div>
+    <div>
+        <label for="passwordConfirmation">
+            Password Confirmation
+        </label>
+        <input
+          wire:model="passwordConfirmation"
+          type="password"
+          id="passwordConfirmation"
+          name="passwordConfirmation"
+        >
+    </div>
+    <div>
+        <button type="submit">Register</button>
+    </div>
+</form>
